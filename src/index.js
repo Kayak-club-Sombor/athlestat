60a391618e8467f792b0ef74592b201e6018a11e
Predrag Drljaca <hi@iampedja.com>
2021-07-19 13:39:30 +0200

    Setting up Tailwind CSS w/ React

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Athlestat</h1>,
-  document.getElementById("root")
-);
+import './index.css'
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById("root"));
